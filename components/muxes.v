f22297e437e7722f6963e31a8b77a0a30f0f7460
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-11-14 18:00:46 -0500

    Adding register file
    Added register file + tests [modified from my HW4]

--- components/muxes.v
+++ components/muxes.v
@@ -1,4 +1,4 @@
-//----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 //  mux_2d
 //      Multiplexer which chooses between two `width`-bit inputs [din0, din1].
 //          WIDTH: `width`, DEPTH: 2
@@ -6,7 +6,7 @@
 //          `width` is a parameter which specifies width, in bits, of each mux
 //          input. The default value of `width` is 32, which corresponds to a
 //          32-bit number.
-//----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 module  mux_2d#(parameter width = 32)(mux_out, mux_ctl, din0, din1);
     input mux_ctl;
     input [w-1:0] din0, din1;
