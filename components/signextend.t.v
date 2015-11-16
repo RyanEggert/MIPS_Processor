79c7714ef5cd5a8a6ecc8375fc6206e942f97743
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-11-16 11:38:48 -0500

    Added sign extend
    Added sign extend module + testbench

---
+++ components/signextend.t.v
@@ -0,0 +1,49 @@
+// signextend.t.v
+`include "signextend.v"
+
+module test_signextend();
+    reg[15:0]   in;
+    wire[31:0]  out;
+
+    reg dutpassed;
+
+    signextend dut (
+                    .in16(in),
+                    .out32(out)
+                    );
+
+
+    initial begin
+        dutpassed = 1;
+        #1
+
+        // Test 1: Sign extend zeroes
+        $display("Starting Test 1 @time = %0dns...", $time);
+        in = 16'b0;
+        #1
+        if (out !=0) begin
+            dutpassed = 0;
+            $display("Test 1 failed.");
+        end
+        #1
+
+        // Test 2: SIgn extend ones
+        $display("Starting Test 2 @time = %0dns...", $time);
+        in = 16'd65535;
+        #1
+        if (out !=32'd4294967295) begin
+            dutpassed = 0;
+            $display("Test 2 failed.");
+        end
+        #1
+
+        #10
+        $display("Tests Complete @time = %0dns.", $time);
+        if (dutpassed == 1) begin
+            $display("Device PASSED");
+        end else begin
+            $display("Device FAILED");
+        end
+    end
+    
+endmodule
---
+++ components/signextend.v
@@ -0,0 +1,16 @@
+//----------------------------------------------------------------------------
+//  Sign
+//      Adds a and b and outputs their sum to sum. The `width` parameter
+//      (default value 32) specifies the width, in bits, of a, b, and sum.
+//   
+//----------------------------------------------------------------------------
+
+module signextend (out32, in16) ;
+    input [15:0] in16;
+    output reg[31:0] out32;
+
+    always @(in16) begin         // Whenever 16-bit input changes,
+        out32 = {{16{in16[15]}},{in16}};  // Precede in16 with 16 of its MSB
+    end
+
+endmodule
