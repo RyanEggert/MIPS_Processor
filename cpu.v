3e85c492c48b5b2df930ad1391dd63f864967a33
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-11-18 23:10:09 -0500

    Debugging CPU

--- cpu.v
+++ cpu.v
@@ -12,20 +12,49 @@
 
 
 module cpu();
-	
-	reg clk;
-	reg reset;
-	wire [31:0] pcin, inst_addr, instr, imm32, shifted_imm32, mux_1_1;
+
+//pc out    + others
+    reg clk;
+    reg reset;
+    wire [31:0] pcin, inst_addr, instr, imm32, shifted_imm32, mux_1_1, adder_pc_sum;
     wire [27:0] shifter_pc_out;
 
+//instruction out 
     wire[5:0] decoded_opcode, decoded_funct;
     wire[4:0] decoded_rs, decoded_rt, decoded_rd, decoded_shamt;
     wire[15:0] decoded_imm16;
     wire[25:0] decoded_address;
 
+//adder out
     wire[31:0]   b;
-    wire[31:0]  adder_pc_sum;
+    wire[31:0]  sum;
     assign b = 32'd4;
+
+//regfile out
+    wire[31:0] ReadData1, ReadData2;
+
+//mux0
+    wire[31:0] mux0out;
+
+//mux5
+    wire[31:0] mux5out;
+
+//alu
+    wire[31:0] ALUres;
+    wire zero, cin, cout, ovf;
+    assign cin = 0;
+
+// control wires
+wire Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, JumpSel, WriDataSel, RegDst;
+
+//datamemory
+    wire[31:0] DataMemOut;
+
+//mux6
+    wire[31:0] mux6out;
+
+//ALUCtrl
+    wire[3:0] ALUCtrlOut;
 
     pc pc_comp (
         .clk(clk),
@@ -66,7 +95,7 @@ module cpu();
         );
 
     shift2 #(.width_in(26), .width_out(28)) shifter_pc  (
-        .out(shifter_pc_out[27:0]),
+        .out(shifter_pc_out),
         .in(decoded_address)
         );
 
@@ -80,10 +109,119 @@ module cpu();
         .in16(decoded_imm16)
         );
 
-    mux_2d mux0 ();
+    mux_2d mux0 (
+    .mux_ctl(JumpSel), //Control
+    .din0({{adder_pc_sum[31:28]}, shifter_pc_out}),
+    .din1(ReadData1),
+    .mux_out(mux0out)
+    );
+
+    and andgate(andout, Branch, zero);
+
+    mux_2d mux1 (
+    .mux_ctl(andout),
+    .din0(adder_pc_sum),
+    .din1(mux_1_1),
+    .mux_out(mux1out)
+    );
+
+    mux_2d mux2 (
+    .mux_ctl(Jump), 
+    .din0(mux1out),
+    .din1(mux0out),
+    .mux_out(mux2out)
+    );
+
+    mux_2d mux3 (
+    .mux_ctl(RegDst), 
+    .din0(decoded_rt),
+    .din1(decoded_rd), 
+    .mux_out(mux3out)
+    );
+
+    mux_2d mux4 (
+    .mux_ctl(Jump),
+    .din0(mux3out),
+    .din1(31),
+    .mux_out(mux4out)
+    );
+
+    mux_2d mux7 (
+    .mux_ctl(WriDataSel), 
+    .din0(adder_pc_sum),
+    .din1(mux0out),
+    .mux_out(mux7out)
+    );
 
 
     initial clk=0;
     always #10 clk =! clk;
 
+
+    regfile regfile (
+    .ReadData1(ReadData1),
+    .ReadData2(ReadData2),
+    .WriteData(mux7out),
+    .ReadRegister1(decoded_rs),
+    .ReadRegister2(decoded_rt),
+    .WriteRegister(mux4out),
+    .RegWrite(RegWrite), 
+    .Clk(clk)
+    );
+
+    mux_2d mux5 (
+    .mux_ctl(ALUSrc), 
+    .din0(ReadData2),
+    .din1(imm32),
+    .mux_out(mux5out)
+    );
+
+    MIPSALU alu_cpu (
+    .alu_ctl(ALUCtrlOut), 
+    .a(ReadData1),
+    .b(mux5out),
+    .cin(cin),
+    .alu_res(ALUres),
+    .zero(ALUzero),
+    .ovf(ovf),
+    .cout(cout)
+    );
+
+    datamemory datamemory_cpu (
+    .clk(clk),
+    .address(ALUres),
+    .data_out(DataMemOut),
+    .write_en(MemWrite), //Control
+    .read_en(MemRead), //Control
+    .data_in(ReadData2)
+    );
+
+    mux_2d mux6 (
+    .mux_ctl(MemtoReg), //Control
+    .din0(ALUres),
+    .din1(DataMemOut),
+    .mux_out(mux6out)
+    );
+
+    ALUControl ALUCtrl (
+    .alu_ctl(ALUCtrlOut),
+    .alu_op(ALUOp) 
+    );
+
+    control control_cpu (
+    .Jump(Jump),
+        .Branch(Branch),
+    .MemRead(MemRead),
+        .MemtoReg(MemtoReg),
+        .MemWrite(MemWrite),
+        .ALUSrc(ALUSrc),
+        .RegWrite(RegWrite),
+        .JumpSel(JumpSel),
+        .RegDst(RegDst),
+        .WriDataSel(WriDataSel),
+        .ALUOp(ALUOp),
+        .opcode(opcode),
+        .funct(funct),
+        .clk(clk)
+        );
 endmodule
